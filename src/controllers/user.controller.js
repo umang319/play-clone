a9af4cb137b08936e44099816f13f87b077d8500
umang319 <umang@evoqtech.com>
2025-11-30 23:25:55 +0530

    access and refresh token learned

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+  try {
+     const user = await User.findById(userId)
+     const accessToken = user.generateAccessToken()
+     const refreshToken = user.generateRefreshToken()
+
+     user.refreshToken = refreshToken
+     await user.save({ validateBeforeSave: false })
+
+     return { accessToken, refreshToken }
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating access and refresh tokens")
+  }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
   // get details from frontend
   const { username, email, fullName, password } = req.body;
@@ -43,7 +58,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
   }
-
+ 
   // upload them to cloudinary, avatar
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
@@ -78,4 +93,88 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req,res) => {
+   // req body => data
+   const {email, username, password} = req.body;
+ 
+   // username or email
+   if(!username || !email) {
+     throw new ApiError(400, "Username or email is required")
+   }
+
+   // find the user
+   const user = await User.findOne({
+    $or: [{username}, {email}]
+   })
+
+   if(!user) {
+    throw new ApiError(404, "User does not exist")
+   }
+
+   // password check 
+   const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if(!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
+   }
+
+   // access and refresh token generation
+   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+   // send cookie
+   const options = {
+      httpOnly: true,
+      secure: true,
+   }
+
+   return res
+   .status(200)
+   .cookie("accessToken", accessToken, options)
+   .cookie("refreshToken", refreshToken, options)
+   .json(
+    new ApiResponse(
+      200,
+      {
+        user: loggedInUser,
+        accessToken,
+        refreshToken
+      },
+      "User logged in successfully"
+    )
+   )  
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+      req.user_id,
+      {
+        $set: {
+          refreshToken: undefined
+        }
+      }, 
+      {
+        new: true
+      }
+    )
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        "User logged out successfully"
+      )
+    )
+})
+
+export { registerUser, loginUser, logoutUser };
