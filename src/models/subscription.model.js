0972eedbe1c9e37afd60aae08e60a59d881ab28a
umang319 <umang@evoqtech.com>
2025-12-01 21:48:59 +0530

    subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionsSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one to who is subscribing
+        ref: "User",
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+        ref: "User"
+    }
+})
+
+export const Subscription = mongoose.model("Subscription", subscriptionsSchema)
